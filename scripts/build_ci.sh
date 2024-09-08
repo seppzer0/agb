a89149a5f04ec04d83b1f00a22f8588111e04870
seppzer0 <s3ppzer0@gmail.com>
2024-09-08 15:24:38 +0300

    scripts: update ci build script

--- scripts/build_ci.sh
+++ scripts/build_ci.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-printf "wip"
+ROOT_DIR=$(dirname $(dirname $(realpath "$0")))
+
+export APP_VERSION=$(bash $ROOT_DIR/scripts/get_version.sh)
+export GO_VERSION=$(go version)
+
+cd $ROOT_DIR/agb
+goreleaser build --snapshot --clean
